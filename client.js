ac4f313020295162934a4a3458da1b7300da3216
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-07-20 18:19:23 -0300

    Set text area disable option

--- client.js
+++ client.js
@@ -1,23 +1,24 @@
 var $name = $("#name");
-var $chat = $('#chat');
+var $chat = $('#chat').prop("disabled", true);
 var client = new Faye.Client('/faye');
+
 // $('#fire').on('click', null, function () { Send message with click
-$(document).keypress(function (e) {
+$(document).keyup(function (e) {
+  if ($name.val() == "") $chat.prop("disabled", true)
+  else if ($name.val() != "") $chat.prop("disabled", false);
   if (e.which == 13) {
     var publication = client.publish('/channel', { text: $name.val() + ": " + $chat.val() });
 
     publication.then(function () {
       console.log('Message received by server!');
-      console.log('Message received by server!');
     }, function (error) {
       alert('There was a problem: ' + error.message);
     });
-
     $chat.val('');
-
     return false;
   }
 });
+
 client.subscribe('/channel', function (message) {
   $('#messages').append('<p>' + message.text + '</p>');
 });
